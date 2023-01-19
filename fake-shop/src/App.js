ff19dbf849683143f5008df690291db11a3b4e7b
Pavithran2 <pavithrank02@gmail.com>
2023-01-19 14:54:36 +0530

    getting data from API call

--- fake-shop/src/App.js
+++ fake-shop/src/App.js
@@ -1,13 +1,23 @@
+import React from 'react';
 import './App.css';
 import Header from './containers/Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <h1> hello</h1>
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+        <Route path='/' exact component={ProductListing} />
+        <Route path='/product/:productId' component={ProductDetail} />
+        <Route> 404 Not found</Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
