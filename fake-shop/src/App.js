ae80f96e991b6d5ae274b81887844b9a696b6b25
Pavithran2 <pavithrank02@gmail.com>
2023-01-19 09:37:25 +0530

    header created

--- fake-shop/src/App.js
+++ fake-shop/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './containers/Header';
 
 function App() {
   return (
     <div className="App">
       <h1> hello</h1>
+      <Header />
     </div>
   );
 }
